11ccb102f5a1ed6bbee7d157fe9396797fe162f2
seerkanyilmazz <seerkanyilmazz@gmail.com>
2021-09-05 18:23:20 +0300

    First commit

---
+++ src/findWordCount/FindWordCount.java
@@ -0,0 +1,19 @@
+package findWordCount;
+
+import java.util.Scanner;
+
+class findWordCount {
+    public static void main(String[] args){
+        int count = 0;
+        System.out.println("Enter your paragraph: ");
+        Scanner scanner = new Scanner(System.in);
+        String sentence = scanner.nextLine();
+
+        for (int i = 0; i < sentence.length(); i++){
+            if (sentence.charAt(i) ==' '){
+                count++;
+            }
+        }
+        System.out.println("Number of words in paragraph: "+(count+1));
+    }
+}
